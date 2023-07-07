781021c84360d24da9dc1f3aa4ab3383cedcd878
daraniamulya <daraniamulya11@gmail.com>
2023-07-07 17:42:15 +0530

    javascript

---
+++ JAVA SCRIPT/data-types.js
@@ -0,0 +1,160 @@
+//numbers
+var a=10;
+console.log(a);
+console.log(typeof a);
+
+var b=15;
+console.log(b);
+console.log(typeof b);
+
+var c=8501;
+console.log(c);
+console.log(typeof c);
+
+var d=6303;
+console.log(d);
+console.log(typeof d);
+
+var e=123456789;
+console.log(e);
+console.log(typeof e);
+
+//strings
+var a1="Ammu";
+console.log(a1);
+console.log(typeof a1);
+
+var a2="chary";
+console.log(a2);
+console.log(typeof a2);
+
+var a3="Sweety";
+console.log(a3);
+console.log(typeof a3);
+
+var a4="Nani";
+console.log(a4);
+console.log(typeof a4);
+
+var a5="Chinnu";
+console.log(a5);
+console.log(typeof a5);
+
+//boolean
+var i=true;
+console.log(i);
+console.log(typeof i);
+
+var j=true;
+console.log(j);
+console.log(typeof j);
+
+var k=false;
+console.log(k);
+console.log(typeof k);
+
+var l=true;
+console.log(l);
+console.log(typeof l);
+
+var m=false;
+console.log(m);
+console.log(typeof m);
+
+//undefined
+var z;
+console.log(z);
+console.log(typeof z);
+
+var y;
+console.log(y);
+
+var x=undefined;
+console.log(x);
+console.log(typeof x);
+
+var w;
+console.log(w);
+console.log(typeof w);
+
+var v=undefined;
+console.log(v);
+
+//null
+var p=null;
+console.log(p);
+console.log(typeof p);
+
+var q=null;
+console.log(q);
+console.log(typeof p);
+
+var r=null;
+console.log(r);
+console.log(typeof r);
+
+var s=null;
+console.log(s);
+console.log(typeof s);
+
+var t=null;
+console.log(t);
+
+//array
+var student=["Amulya","Naveen","Arun"];
+console.log(student);
+console.log(typeof student);
+
+var names=["Ramana","Revathi"];
+console.log(names);
+
+var cources=["PGDCA","DCA"];
+console.log(cources);
+
+var flowers=["Rose","Lotus","Lilly","Jasmine"];
+console.log(flowers);
+
+var subjects=["Maths-1","Maths-2","Physics"];
+console.log(subjects);
+
+//objects
+const data_1={
+
+    Name:"Amulya",
+    Address:"Ramagundam",
+    Number:123456789,
+   };
+  console.log(data_1);
+
+const data_2={
+
+    Name:"Naveen",
+    age:23,
+    address:"Godavarikhani"
+};
+console.log(data_2);
+
+var data_3={
+
+    Name:"Arun",
+    age:19,
+    address:"Godavarikhani"
+};
+console.log(data_3);
+
+var data_4={
+
+    Name:"Sweety",
+    age:21,
+    address:"Karimnagar"
+};
+console.log(data_4);
+
+let data_5={
+
+    Name:"Chary",
+    age:23,
+    number:7890450002,
+};
+console.log(data_5);
+
